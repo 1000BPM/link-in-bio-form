cb5b3b6cfb5a4dafb5c46d606c2f3115557ff2c1
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-20 02:48:14 +0000

    Final draft

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,22 @@ class ItemsController < ApplicationController
 
   def add
     
-    render ({ :template => "item_templates/form"})
+    render({ :template => "item_templates/form"})
+  end
+
+  def post_new
+
+  url_add = params.fetch("link_url") 
+  desc_add = params.fetch("link_desc")
+  img_add = params.fetch("image_url")
+
+  i = Item.new
+
+  i.link_url = url_add
+  i.link_description = desc_add
+  i.thumbnail_url = img_add
+  i.save
+
+  redirect_to("/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get("/", { :controller => "items", :action => "index" })
 
   get("/backdoor", { :controller => "items", :action => "add"})
+
+  post("/add_results", { :controller => "items", :action => "post_new"})
+  
 end
